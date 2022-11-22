2015c4a576584552ee67ee4e71cc42467629eb19
Roan Kirwin <kirwinr@mkmorse.com>
2022-11-22 17:55:59 +0000

    Updates

---
+++ precache-manifest.59808eb1fe5d4dfd274b62476d61b2e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe26049cdec4e21d80eb5bfa5f58757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57e9de127cddf78a4250",
+    "url": "/static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "c326da05f57f8ca34d78",
+    "url": "/static/js/2.df0f56fa.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/static/js/2.df0f56fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e9de127cddf78a4250",
+    "url": "/static/js/main.c5a83f18.chunk.js"
+  },
+  {
+    "revision": "8a048bcfb450c5047a9d",
+    "url": "/static/js/runtime-main.2a2b5a60.js"
+  }
+]);
