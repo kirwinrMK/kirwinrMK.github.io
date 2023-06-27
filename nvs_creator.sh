66d0254ab46baa32e32656ff9dd27b5a5eade1d2
Roan Kirwin <kirwinr@mkmorse.com>
2023-06-27 20:14:45 +0000

    deploy

--- nvs_creator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Creating NVS partition"
-# 1 is input .csv
-# 2 is output .bin
-echo $1
-echo $2
-python /home/administrator/Documents/Espressif/esp-idf/components/nvs_flash/nvs_partition_generator/nvs_partition_gen.py generate $1 $2 0x3000
